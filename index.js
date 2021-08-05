af25778af5b5407cd0bd9ccec5656d9466135c41
Zachary King <zach_a_king@outlook.com>
2021-08-05 13:32:01 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const util = require("util");
+const chalk = require("chalk");
+const path = require("path");
+
+const { lstat } = fs.promises;
+
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
+  if (err) {
+    console.log(err);
+  }
+
+  const statPromises = filenames.map((filename) => {
+    return lstat(path.join(targetDir, filename));
+  });
+
+  const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    if (stats.isFile()) {
+      console.log(chalk.red(filenames[index]));
+    } else {
+      console.log(chalk.bold.cyan(filenames[index]));
+    }
+  }
+});
